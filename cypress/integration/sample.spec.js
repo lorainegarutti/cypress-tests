36a73e0dfb556e6106f8bafd742331e95e932092
Loraine Garutti <60954622+lorainegarutti@users.noreply.github.com>
2020-03-16 12:51:19 -0300

    finalização de cenários e testes

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,32 +1,22 @@
 describe("Cenário 1", function() {
-    it("Acessar informações sobre o livro 'Código Limpo'", function() {
+    it("Exibir resultados para o livro 'Código Limpo'", function() {
         cy.visit("https://americanas.com.br")
-        
-        cy.pause()
 
-        cy.get("#h_search-input")
-          .type("livro codigo limpo")
-          .should("have.value", "livro codigo limpo")
-        
-        cy.get("#h_search-btn")
-          .click()
-
-        cy.get("picture > img[alt='Livro - Código Limpo: Habilidades Práticas do Agile Software']")
-          .click()        
+        cy.get("#h_search-input").type("livro codigo limpo").should("have.value", "livro codigo limpo")
+        cy.get("#h_search-btn").click()       
+    })
+    it("Acessar o primeiro item da busca", function() {
+        cy.get("picture > img[alt='Livro - Código Limpo: Habilidades Práticas do Agile Software']").click()        
     })
 })
 
 describe("Cenário 2", function() {
     it("Calculando frete e prazo", function() {
-        cy.get("#freight-field")
-          .type("04055-000")
-          .should("have.value", "04055-000")
-        cy.get("#freight-field-button")
-          .click()
+        cy.get("#freight-field").type("04055-000").should("have.value", "04055-000")
+        cy.get("#freight-field-button").click()
     })
 
     it("Colocar o livro no carrinho", function() {
-        cy.get("button[class='ButtonUI-sc-1i9za0i-0 erKOhv']")
-          .click({force:true})
+        cy.get("#btn-buy").click({force:true})
     })
 })
